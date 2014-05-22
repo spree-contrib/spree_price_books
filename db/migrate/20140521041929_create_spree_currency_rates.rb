c075c77f7f08f36ef71721c8576197fd90c8994d
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-05-22 01:53:14 -0400

    Should index default.

--- db/migrate/20140521041929_create_spree_currency_rates.rb
+++ db/migrate/20140521041929_create_spree_currency_rates.rb
@@ -3,7 +3,7 @@ class CreateSpreeCurrencyRates < ActiveRecord::Migration
     create_table :spree_currency_rates do |t|
       t.string :base_currency
       t.string :currency
-      t.boolean :default, null: false, default: false
+      t.boolean :default, null: false, default: false, index: true
       t.float :exchange_rate
       t.timestamps
     end
